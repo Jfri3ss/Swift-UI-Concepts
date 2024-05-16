0a73d4caa558b01ee839e67b375402358f5c236f
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-05-16 13:46:14 -0400

    Create Mini_Weather_ApplicationApp.swift

---
+++ Mini_Weather_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_Weather_ApplicationApp.swift
+//  Mini Weather Application
+//
+//  Created by Steve Jobs on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini_Weather_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
